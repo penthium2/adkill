c8194823961ed54af47514be810ae1ff38f9e83b
penthium2 <contact@viperr.org>
2016-03-11 14:00:07 +0100

    Add new features
    add new sources files.

--- README.md
+++ README.md
@@ -9,7 +9,20 @@ Inspired by original [hakerdefo] script.
 This script generates a powerful Hosts file for linux by merging 5 lists of advertisement references.
 
 # Usage :
-Just run the script.
+Just run the script or add some options :
+- --ats : activate blacklist of ad/tracking servers listed in the hpHosts database.
+- --emd : activate blacklist of malware sites listed in the hpHosts database.
+- --exp : activate blacklist of exploit sites listed in the hpHosts database.
+- --fsa : activate blacklist of fraud sites listed in the hpHosts database.
+- --grm : activate blacklist of sites involved in spam (that do not otherwise meet any other classification criteria) listed in the hpHosts database.
+- --hfs : activate blacklist of sites spamming the hpHosts forums (and not meeting any other classification criteria) listed in the hpHosts database.
+- --hjk : activate blacklist of hijack sites listed in the hpHosts database.
+- --mmt : activate blacklist of sites involved in misleading marketing (e.g. fake Flash update adverts) listed in the hpHosts database.
+- --pha : activate blacklist of illegal pharmacy sites listed in the hpHosts database.
+- --psh : activate blacklist of phishing sites listed in the hpHosts database.
+- --wrz : activate blacklist of warez/piracy sites listed in the hpHosts database.
+- --all : activate blacklist of all sites listed in the hpHosts database.
+
 
 Adkill creates ~/.adkill directory then
 - backs up original /etc/hosts file named : **hosts-system**
--- adkill.sh
+++ adkill.sh
@@ -10,7 +10,6 @@
 ############################################################
 # Method for animation while script is working:
 # to call it, use : spinner & ; pidspin=$(jobs -p) ; disown
-
 spinner() {
     local i sp n
     sp='/-\|'
@@ -25,6 +24,99 @@ killspinner() {
 kill $pidspin 
 printf "\n"
 }
+## download checker :
+donloadchecker() {
+if [[ $? = 0 ]] ; then
+	echo "Download completed"
+	else
+	echo "Download Error"
+fi
+}
+############################################################
+#
+#set variable to default setting for diferrents categories :
+ats=0
+emd=0
+exp=0
+fsa=0
+grm=0
+hfs=0
+hjk=0
+mmt=0
+pha=0
+psh=0
+wrz=0
+while [[ -n "$1" ]]
+	do
+	case $1 in 
+	--ats)
+		ats=1
+	;;
+	--emd)
+		emd=1
+	;;
+	--exp)
+		exp=1
+	;;
+	--fsa)
+		fsa=1
+	;;
+	--grm)
+		grm=1
+	;;
+	--hfs)
+		hfs=1
+	;;
+	--hjk)
+		hjk=1
+	;;
+	--mmt)
+		mmt=1
+	;;
+	--pha)
+		pha=1
+	;;
+	--psh)
+		psh=1
+	;;
+	--wrz)
+		wrz=1
+	;;
+	--all)
+		ats=1
+		emd=1
+		exp=1
+		fsa=1
+		grm=1
+		hfs=1
+		hjk=1
+		mmt=1
+		pha=1
+		psh=1
+		wrz=1
+	;;
+
+	*)
+		echo "syntax error"
+		echo "run adkill.sh <option>"
+		echo "--ats : activate blacklist of ad/tracking servers listed in the hpHosts database."
+		echo "--emd : activate blacklist of malware sites listed in the hpHosts database."
+		echo "--exp : activate blacklist of exploit sites listed in the hpHosts database."
+		echo "--fsa : activate blacklist of fraud sites listed in the hpHosts database."
+		echo "--grm : activate blacklist of sites involved in spam (that do not otherwise meet any other classification criteria) listed in the hpHosts database."
+		echo "--hfs : activate blacklist of sites spamming the hpHosts forums (and not meeting any other classification criteria) listed in the hpHosts database."
+		echo "--hjk : activate blacklist of hijack sites listed in the hpHosts database."
+		echo "--mmt : activate blacklist of sites involved in misleading marketing (e.g. fake Flash update adverts) listed in the hpHosts database."
+		echo "--pha : activate blacklist of illegal pharmacy sites listed in the hpHosts database."
+		echo "--psh : activate blacklist of phishing sites listed in the hpHosts database."
+		echo "--wrz : activate blacklist of warez/piracy sites listed in the hpHosts database."
+		echo "--all : activate blacklist of all sites listed in the hpHosts database."
+		exit 1
+	;;
+	esac
+	shift
+done
+
 ###########################################################
 
 
@@ -41,44 +133,92 @@ fi
 temphosts1=$(mktemp)
 temphosts2=$(mktemp)
 temphosts3=$(mktemp)
-
+##########################################################
+#
 # Obtain various hosts files and merge into one
 echo "Downloading ad-blocking hosts files..."
 echo "Downloading from : winhelp2002.mvps.org :"
 wget -nv -O - http://winhelp2002.mvps.org/hosts.txt >> "$temphosts1"
-if [[ $? = 0 ]] ; then
-	echo "Download completed"
-	else
-	echo "Download Error"
-fi
+donloadchecker
 echo "Downloading from hosts-file.net :"
-wget -nv -O - http://hosts-file.net/ad_servers.asp >> "$temphosts1"
-if [[ $? = 0 ]] ; then
-        echo "Download completed"
-        else
-        echo "Download Error"
+if [[ "$ats" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : ad/tracking servers"
+	wget -nv -O - http://hosts-file.net/ad_servers.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$emd" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : malware sites"
+	wget -nv -O - http://hosts-file.net/emd.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$exp" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : exploit sites"
+	wget -nv -O - http://hosts-file.net/exp.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$fsa" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : fraud sites"
+	wget -nv -O - http://hosts-file.net/fsa.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$grm" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : sites involved in spam"
+	wget -nv -O - http://hosts-file.net/grm.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$hfs" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : sites spamming the hpHosts forums"
+	wget -nv -O - http://hosts-file.net/hfs.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$hjk" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : hijack sites"
+	wget -nv -O - http://hosts-file.net/hjk.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$mmt" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : sites involved in misleading marketing"
+	wget -nv -O - http://hosts-file.net/mmt.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$pha" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : illegal pharmacy sites"
+	wget -nv -O - http://hosts-file.net/pha.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$phs" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : phishing sites"
+	wget -nv -O - http://hosts-file.net/psh.txt >> "$temphosts1"
+	donloadchecker
+fi
+if [[ "$wrz" = 1 ]] 
+	then
+	echo "Downloading from hosts-file.net : warez/piracy"
+	wget -nv -O - http://hosts-file.net/wrz.txt >> "$temphosts1"
+	donloadchecker
 fi
 echo "Downloading from someonewhocares.org :"
 wget -nv -O - http://someonewhocares.org/hosts/hosts >> "$temphosts1"
-if [[ $? = 0 ]] ; then
-        echo "Download completed"
-        else
-        echo "Download Error"
-fi
+donloadchecker
 echo "Downloading from pgl.yoyo.org :"
 wget -nv -O - "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext" >> "$temphosts1"
-if [[ $? = 0 ]] ; then
-        echo "Download completed"
-        else
-        echo "Download Error"
-fi
+donloadchecker
 echo "Downloading from downloads.sourceforge.net/project/adzhosts :"
 wget -nv -O - "http://downloads.sourceforge.net/project/adzhosts/FORADAWAY.txt"  >> "$temphosts1"
-if [[ $? = 0 ]] ; then
-        echo "Download completed"
-        else
-        echo "Download Error"
-fi
+donloadchecker
+
+
+read -p "STOP HERE"
 # Do some work on the file:
 printf "Parsing, cleaning, de-duplicating, sorting..."
 #sed -e 's/\r//' -e '/localhost/d' -e 's/127.0.0.1/0.0.0.0/' -e 's/#.*$//' -e 's/[ \s]*$//' -e '/^$/d' -e 's/\s/ /g' -e '/^[^0]/d' "$temphosts1" | sort -u > "$temphosts2"
